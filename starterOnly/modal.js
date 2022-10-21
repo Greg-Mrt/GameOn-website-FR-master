d2084bc816b6f20f8b07227ace2b8a6e925a9fbd
Gregoire Martin <g.martin@langloys.com>
2022-10-21 17:13:31 +0200

    add validations

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,50 @@ function launchModal() {
 function closeModal (){
   modalbg.style.display = "none";
 }
+
+
+//check first name data
+function checkFirstName() {
+  let regex = /^[a-z]+$/;
+	let inputValue = document.getElementById("first").value;
+	if (inputValue !== null && inputValue.length > 2) return true;
+	else return regex.test(inputValue);
+}
+
+function checkLastName() {
+	let inputValue = document.getElementById("last").value;
+	if (inputValue !== null && inputValue.length > 2) return true;
+	else return false;
+}
+
+function checkEmail() {
+    let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,4})$/;
+    let inputValue = document.getElementById("email").value;
+    return regex.test(inputValue);
+}
+
+function checkQuantity() {
+    let regex = /^[0-9]+$/;
+    let inputValue = document.getElementById("quantity").value;
+    return regex.test(inputValue);
+}
+
+function checklocation() {
+    let radioButtons = document.querySelectorAll(".checkbox-input[type=radio]");
+    for(let radio of radioButtons){
+        if(radio.checked === true) return true;
+    }
+    return false;
+}
+
+function verifcheckbox() {
+    let inputValue = document.getElementById("checkbox1").checked;
+    return inputValue;
+}
+
+document
+	.getElementById("button")
+	.addEventListener("click", function checkForm(event) {
+		event.preventDefault();
+		console.log(verifcheckbox());
+	});
